58c85da1cc91b7c1c468b70a75eafc46895bf5d3
Ritika86240 <146440022+Ritika86240@users.noreply.github.com>
2023-10-27 17:58:12 +0530

    Add files via upload

---
+++ NEW.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int name,register number;
+    printf( " your name");
+    scanf("%name \n", &name);
+    printf(" enter your register number ");
+    scanf(" %d \t " & register number)
+
+}
+
+
+
+
+
+
+
+
+
