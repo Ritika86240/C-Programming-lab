2595af4e140bddf8d04737af47e0b7b906d8e10f
Ritika86240 <146440022+Ritika86240@users.noreply.github.com>
2023-10-30 18:19:07 +0530

    Add files via upload

---
+++ HELLOWORLD.c
@@ -0,0 +1,31 @@
+/*  // program to print hello world
+#include<stdio.h>
+void main()
+{  int hello world;
+printf(" hello world");
+
+}*/
+
+/* // program to print hello world using tab character
+
+#include<stdio.h>
+void main()
+{
+    int hello world
+ printf(" hello world\t");
+
+}*/
+
+/*  // program to print hello world ussing new line character
+
+#include<stdio.h>
+void main()
+{
+    int hello world;
+    printf(" hello world");
+}
+*/
+
+
+
+
